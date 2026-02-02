ae80ce3653c66f6d43407a0c6026654d716cfa00
devanshijodhani <devanshijodhani45@gmai.com>
2026-02-02 18:09:29 +0530

    add: components streaming

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,46 +1,10 @@
-import { Button } from '@/components/ui/button';
-import { Tabs, TabsContent, TabsTrigger, TabsList } from '@/components/ui/tabs';
 import Image from 'next/image';
-import ProductCard from './components/product-card';
-import { Category, Product } from '@/lib/types';
+import { Button } from '@/components/ui/button';
+import ProductList from './components/product-list';
+import { Suspense } from 'react';
+import ProductListSkeleton from './components/product-list-skeleton';
 
 export default async function Home() {
-  // todo: Add promis all
-  // Fetching Categories
-  const categoryResponse = await fetch(
-    `${process.env.BACKEND_URL}/api/catalog/categories`,
-    {
-      next: {
-        revalidate: 3600, // 1 hour
-      },
-    },
-  );
-
-  if (!categoryResponse.ok) {
-    throw new Error('Failed to fetch categories');
-  }
-
-  const data = await categoryResponse.json();
-  const categories: Category[] = data.categories;
-
-  // Fetching Products
-  const productsResponse = await fetch(
-    // TODO: Add dynamic tenantId
-    `${process.env.BACKEND_URL}/api/catalog/products?perPage=100&tenantId=3`,
-
-    {
-      next: {
-        revalidate: 3600, // 1 hour
-      },
-    },
-  );
-
-  if (!productsResponse.ok) {
-    throw new Error('Failed to fetch categories');
-  }
-
-  const products: { data: Product[] } = await productsResponse.json();
-
   return (
     <>
       <section className="bg-white">
@@ -67,40 +31,9 @@ export default async function Home() {
           </div>
         </div>
       </section>
-      <section>
-        <div className="max-w-330 px-6 mx-auto py-12">
-          <Tabs defaultValue={categories[0]._id}>
-            <TabsList>
-              {categories.map((category) => {
-                return (
-                  <TabsTrigger
-                    key={category._id}
-                    value={category._id}
-                    className="text-md">
-                    {category.name}
-                  </TabsTrigger>
-                );
-              })}
-            </TabsList>
-
-            {categories.map((category) => {
-              return (
-                <TabsContent key={category._id} value={category._id}>
-                  <div className="grid grid-cols-4 gap-6 mt-6">
-                    {products.data
-                      .filter(
-                        (product) => product.category._id === category._id,
-                      )
-                      .map((product) => (
-                        <ProductCard product={product} key={product._id} />
-                      ))}
-                  </div>
-                </TabsContent>
-              );
-            })}
-          </Tabs>
-        </div>
-      </section>
+      <Suspense fallback={<ProductListSkeleton />}>
+        <ProductList />
+      </Suspense>
     </>
   );
 }
