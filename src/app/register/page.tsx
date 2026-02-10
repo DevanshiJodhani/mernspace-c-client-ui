0dc53869059508dfd42a7355bd8a4f7fff129429
devanshijodhani <devanshijodhani45@gmai.com>
2026-02-10 19:22:51 +0530

    add: register page UI

---
+++ src/app/register/page.tsx
@@ -0,0 +1,68 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const SignUpPage = () => {
+  return (
+    <div className="w-full lg:grid lg:min-h-150 lg:grid-cols-2 xl:min-h-200">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-87.5 gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Signup</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your information to create an account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">First name</Label>
+                <Input id="first-name" placeholder="Max" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Last name</Label>
+                <Input id="last-name" placeholder="Robinson" required />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <Button type="submit" className="w-full">
+              Register
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{' '}
+            <Link href="/login" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/login-image.webp"
+          width={1920}
+          height={1080}
+          style={{ objectFit: 'cover' }}
+          alt="Image"
+          className="h-screen"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
